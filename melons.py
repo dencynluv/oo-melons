334f4728e155c63a117a38a7fe8b363f1f361f03
Hackbright Student <info@hackbrightacademy.com>
2016-04-15 12:50:35 -0700

    Added InternationalMelonOrder flat fee of

--- melons.py
+++ melons.py
@@ -32,6 +32,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     # order_type = "domestic"
     # tax = 0.08
 
+
     def __init__(self, species, qty): #number of arguments we enter when the class is being instantiated
         super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08, "USA") #These parameters need to match super class parameters)
         # because they are the ones being passed into the AbstractMelonOrder(parent)
@@ -49,6 +50,17 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code): # When we instanciate InternationalMelonOrder these are the parameters we need provide arguments for.
         super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17, country_code)  #arguments
         # Because we are calling the parent class in this dunder init, the arguments neeed to match the parent class parameters.
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.qty < 10:
+            total += 3
+
+        return total
 
     # need to add a dunder init like above or undo the dunder init above in order for the bottom to work. 
     # both ways work
